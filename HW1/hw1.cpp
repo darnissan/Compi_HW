41d639372f1fde6e35bd3225363eb3e3b22d2aa2
Dar Nissan <darnis18@gmail.com>
2024-01-24 11:56:08 +0200

    fixing errors so that string token isnt printed until valdiatin THE WHOLE string

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -1,77 +1,112 @@
 #include "tokens.hpp"
+#include <iostream>
 #include <string>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+
 using namespace std;
 
-void string_handler(string str);
-string trimFirst_N_Last(string str);
-void backslashN_handler();
-void backslashR_handler();
-void backslashT_handler();
-void doubleBackslash_handler();
-void backslashQuote_handler();
-void AsciiEscape_handler(string num);
+string trimFirst_N_Last(const string &str);
+void handleAsciiEscape(const string &num, string &result);
 
-string trimFirst_N_Last(string str)
+string trimFirst_N_Last(const string &str)
 {
 	if (str.length() > 2)
 	{
 		return str.substr(1, str.length() - 2);
 	}
-	else
+	return "";
+}
+
+void handleAsciiEscape(const string &num, string &result)
+{
+	long ascii = strtol(num.c_str(), nullptr, 16);
+	if ((ascii == 0 && num != "00") || ascii > 0x7F)
 	{
-		return "";
-	}
-}
-
-void backslashN_handler()
-{
-
-	printf("\n");
-}
-
-void backslashR_handler()
-{
-	printf("\r");
-}
-
-void backslashT_handler()
-{
-	printf("\t");
-}
-
-void doubleBackslash_handler()
-{
-	printf("\\");
-}
-
-void backslashQuote_handler()
-{
-	printf("\"");
-}
-
-void AsciiEscape_handler(string num)
-{
-	string hex = num;
-	int ascii = strtol(hex.c_str(), NULL, 16);
-	if (ascii == 0 && hex != "00")
-	{
-		printf("Error undefined escape sequence %s\n", hex.c_str());
+		cout << "Error undefined escape sequence x" << num << endl;
 		exit(0);
 	}
-	string max_ascii = "0x7F";
-	if (ascii > strtol(max_ascii.c_str(), NULL, 16))
-	{
-		printf("Error undefined escape sequence %s\n", hex.c_str());
-		exit(0);
-	}
-	printf("%c", ascii);
+	result += static_cast<char>(ascii);
 }
-void backslashZero_handler()
+
+void string_handler(const string &str)
 {
-	string str = "\0";
-	printf("%s", str.c_str());
+	string to_print = to_string(yylineno) + " STRING ";
+	string trimmed = trimFirst_N_Last(str);
+	size_t index = 0;
+	size_t length = trimmed.length();
+
+	while (index < length)
+	{
+		if (trimmed[index] == '\\')
+		{
+			if (index + 1 < length)
+			{
+				switch (trimmed[index + 1])
+				{
+				case 'n':
+					to_print += "\n";
+					break;
+				case 'r':
+					to_print += "\r";
+					break;
+				case 't':
+					to_print += "\t";
+					break;
+				case '\\':
+					to_print += "\\";
+					break;
+				case '"':
+					to_print += "\"";
+					break;
+				case 'x':
+					if (index + 3 < length)
+					{
+						handleAsciiEscape(trimmed.substr(index + 2, 2), to_print);
+						index += 3; // Skip the x and two hex digits
+					}
+					else
+					{
+						cout << "Error undefined escape sequence " << trimmed[index + 1];
+						if (index + 2 < length)
+							cout << trimmed[index + 2];
+						cout << endl;
+						exit(0);
+					}
+					break;
+				case '0':
+					to_print += "\0";
+					break;
+				default:
+					cout << "Error undefined escape sequence " << trimmed[index + 1] << endl;
+					exit(0);
+				}
+				index += 2;
+			}
+			else
+			{
+
+				cout << "Error unclosed string" << endl;
+				exit(0);
+			}
+		}
+		else if (trimmed[index] == '\n')
+		{
+
+			cout << "Error unclosed string" << endl;
+			exit(0);
+		}
+		else if (trimmed[index] == '\r')
+		{
+
+			cout << "Error unclosed string" << endl;
+			exit(0);
+		}
+		else
+		{
+			to_print += trimmed[index++];
+		}
+	}
+	cout << to_print << endl;
 }
 
 int main()
@@ -79,21 +114,19 @@ int main()
 	int token;
 	while ((token = yylex()))
 	{
-
 		switch (token)
 		{
 		case STRING:
 		{
 			string str_token = yytext;
 			string_handler(str_token);
-		}
-		break;
-		case COMMENT:
-			printf("%d COMMENT //\n", yylineno);
 			break;
-		// Dealing with illegal chars
+		}
+		case COMMENT:
+			cout << yylineno << " COMMENT //" << endl;
+			break;
 		case UNKNOWN:
-			printf("Error %c\n", *yytext);
+			cout << "Error " << *yytext << endl;
 			exit(0);
 			break;
 		default:
@@ -102,85 +135,4 @@ int main()
 		}
 	}
 	return 0;
-}
-
-void string_handler(string str)
-{
-	printf("%d STRING ", yylineno);
-	str = trimFirst_N_Last(str);
-	int index = 0;
-	while (index < str.length())
-	{
-		if (str[index] == '\\')
-		{
-			if (index + 1 < str.length())
-			{
-				switch (str[index + 1])
-				{
-				case 'n':
-					backslashN_handler();
-					index += 2;
-					break;
-				case 'r':
-					backslashR_handler();
-					index += 2;
-					break;
-				case 't':
-					backslashT_handler();
-					index += 2;
-					break;
-				case '\\':
-					doubleBackslash_handler();
-					index += 2;
-					break;
-				case '"':
-					backslashQuote_handler();
-					index += 2;
-					break;
-				case 'x':
-					if (index + 3 < str.length())
-					{
-						AsciiEscape_handler(str.substr(index + 2, 2));
-						index += 4;
-					}
-					else
-					{
-						printf("Error undefined escape sequence %c", str[index + 1]);
-						if (index + 2 < str.length())
-							printf("%c", str[index + 2]);
-						printf("\n");
-						exit(0);
-					}
-					break;
-				case '0':
-					backslashZero_handler();
-					index += 2;
-					break;
-				default:
-
-					printf("Error undefined escape sequence %c\n", str[index + 1]);
-					exit(0);
-				}
-			}
-			else if (str[index] == '\n')
-			{
-				printf("Error unclosed string\\n\n");
-				exit(0);
-			}
-			else
-			{
-				// If '\' is the last character,it's an error, exit the program
-				printf("Error unclosed string\n");
-				exit(0);
-			}
-		}
-
-		else
-		{
-			// Print characters other than backslash as they are
-			printf("%c", str[index]);
-			index++;
-		}
-	}
-	printf("\n");
 }
